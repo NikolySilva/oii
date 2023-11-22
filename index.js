8567439e5bcfe1f2b0afbd15c090f5f2aad0e59b
NikolySilva <nikolypereira72@gmail.com>
2023-11-22 17:57:15 -0300

    first commit

--- index.js
+++ index.js
@@ -6,6 +6,41 @@ function sendOTP(){
 
     //Create a SMTP credentials
 
+    // generating random number as OTP;
+    let otp_val = Math.floor(Math.random()*10000);
+
+    let emailbody = `
+        <h1>Verificação</h1><br>
+        <h2>Seu código é</h2>${otp_val}
+    `;
     //email code
-    ser´q que agora vai
+    Email.send({
+        SecureToken : "4b2d5094-3c29-4fbc-91e1-45169f4d3528",
+        To : email.value,
+        From : "nikolypereira72@gmail.com",
+        Subject : "Olá segue abaixo seu código:",
+        Body : emailbody
+    }).then(
+        //se sucesso retorna "ok"
+      message => {
+        if(message == "ok"){
+            alert("Código enviado para o email "+ email.value);
+
+            //agora deixar o output visible
+            otpverify.computedStyleMap.display = "block";
+            const otp_inp = document.getElementById('otp_inp');
+            const otp_btn = document.getElementById('otp-btn');
+
+            otp_btn.addEventListener('click' ,()=>{
+                //agora verifica email é valido
+                if(otp_inp.value == otp_val){
+                    alert("Email verificado");
+                }
+                else{
+                    alert("Invalido OTP");
+                }
+            })
+        }
+      }
+    );
 }
