cf2205d80e79b699e909ff06645a9d6da89d710b
NikolySilva <nikolypereira72@gmail.com>
2023-11-22 17:11:39 -0300

    teste 2

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+function sendOTP(){
+    const email = document.getElementById('email');
+    const otpverify = document.getElementsByClassName('otpverify')[0];
+
+    //Create a SMTP credentials
+
+    //email code
+}
